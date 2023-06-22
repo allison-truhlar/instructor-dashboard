bf99a97d88707ade5cae94b1ee7343acd51f24dc
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-06-21 21:04:00 -0400

    Fixed frontend to backend connection
    Had removed app.listen(port). This disconnected the two.

--- backend/server.js
+++ backend/server.js
@@ -5,19 +5,17 @@ const port = 4001
 const canvasAPI = require('node-canvas-api')
 
 app.use(cors())
+app.listen(port)
 
 // Make endpoint for getSelf here
 app.get('/getSelf', async (req, res) => {
-  // we need to now make a call to the Canvas API,
-  // wait for the response, then send the result to the frontend
+  // make a call to the Canvas API, wait for the response, then send the result to the frontend
   const self = await canvasAPI.getSelf()
   res.json(self)
 })
 
-// Make endpoint for getCoursesByUser here
+// Endpoint - get courses by user
 app.get('/getCoursesByUser/:id', async (req, res) => {
-  // we need to now make a call to the Canvas API,
-  // wait for the response, then send the result to the frontend
   const userId = req.params.id
   const courses = await canvasAPI.getCoursesByUser(userId)
   res.json(courses)
@@ -25,8 +23,6 @@ app.get('/getCoursesByUser/:id', async (req, res) => {
 
 // Endpoint - assignments
 app.get('/getAssignments/:id', async (req, res) => {
-  // we need to now make a call to the Canvas API,
-  // wait for the response, then send the result to the frontend
   const courseId = req.params.id
   const assignments = await canvasAPI.getAssignments(courseId)
   res.json(assignments)
@@ -34,7 +30,6 @@ app.get('/getAssignments/:id', async (req, res) => {
 
 // Endpoint - bulk user progress
 app.get('/getMultipleAssignmentSubmissions/:id', async (req, res) => {
-  //NOTE - requires an option of ?student_ids[]=all
   const courseId = req.params.id
   const options = req.query
   const multipleAssignmentSubmissions = await canvasAPI.getMultipleAssignmentSubmissions(courseId, options)
@@ -43,18 +38,16 @@ app.get('/getMultipleAssignmentSubmissions/:id', async (req, res) => {
 
 // Endpoint- getUsers 
 app.get('/getUsersInCourse/:id', async (req, res) => {
-  // we need to now make a call to the Canvas API,
-  // wait for the response, then send the result to the frontend
   const courseId = req.params.id
   const options = req.query
   const users = await canvasAPI.getUsersInCourse(courseId, options)
   res.json(users)
 })
 
-  // Endpoint - getCourse 
-app.get('/getCourse', async (req, res) => {
-  // we need to now make a call to the Canvas API,
-  // wait for the response, then send the result to the frontend
-  const course = await canvasAPI.getCourse(6880352)
-  res.json(course)
-})
+//   // Endpoint - getCourse 
+// app.get('/getCourse', async (req, res) => {
+//   const course = await canvasAPI.getCourse(6880352)
+//   res.json(course)
+// })
+
+
